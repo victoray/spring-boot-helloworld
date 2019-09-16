6ecb589c6da2b3b91d2d37cd95643223b1bc8b8b
Victor Ay <viktoray007@gmail.com>
2019-09-16 13:19:15 +0300

    add statics

--- src/main/java/com/odinson/springboothelloworld/servlet/HelloFilter.java
+++ src/main/java/com/odinson/springboothelloworld/servlet/HelloFilter.java
@@ -8,8 +8,7 @@ import java.io.IOException;
 public class HelloFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest servletRequest,
-                         ServletResponse servletResponse, FilterChain filterChain)
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
         System.out.println("Executing doFilter method");
         filterChain.doFilter(servletRequest, servletResponse);
